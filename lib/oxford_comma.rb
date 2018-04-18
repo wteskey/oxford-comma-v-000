03f84910b5a54d81d6645ecbceff2993801b0735
William J Teskey <willjteskey@gmail.com>
2018-04-18 19:00:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ if array.length == 1
+  array.join
+  elsif array.length == 2
+  array.join(" and ")
+else array.length == 3 || array.length > 3
+  a = array.pop
+  array.join(", ") + ", and " + a
 end
+end
+
+
